e3c301054f7fc0f2e6ed820fdd972450e6c90830
devinallen-07 <devinallen@defenseunicorns.com>
2024-06-12 12:32:20 -0600

    First Commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World! This is YOUR APP YOUR ENVIRONMENT...YAYE');
+});
+
+app.listen(3030, () => {
+  console.log('Server started on port 3030');
+});
